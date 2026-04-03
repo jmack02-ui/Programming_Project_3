55cb6f93bab677f087f7fd2d49b5e0db0c420070
Keanu Cruz <kcruz4@bruinmail.slcc.edu>
2026-04-03 10:14:22 -0600

    Initial Boilercode for Tests

---
+++ src/test/strategies/BruteForceStrategies.java
@@ -0,0 +1,52 @@
+package strategies;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+/**
+ * Test Suite for BruteForce.java
+ * @author Keanu Cruz
+ */
+public class BruteForceStrategies {
+
+    @Test
+    void standardOperation(){
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void lessThanThreeSubsets(){
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void optimalCase(){
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void subsetCountValidation(){
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void multipleSubsetsSameRating(){
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void subsetsInBounds(){
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void noValidSubsets(){
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void emptySubsets(){
+        fail("Not yet implemented");
+    }
+}
